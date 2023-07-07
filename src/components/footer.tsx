3052d4bb515e5efdab9aed630eff60071c0093dc
Jeremy Kintana <81502213+jkintana@users.noreply.github.com>
2023-07-06 20:56:06 -0700

    basic header/footer content
    Nothing set in stone, just getting used to NextJS.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,8 @@
 export default function Footer() {
   return (
-    <div id="footer"></div>
+    <div id="footer">
+      <p className="text-center">Contact us at <a href="mailto:contact@waisi.org">contact@waisi.org</a>.</p>
+      <p className="text-center">In collaboration with <a href="https://eauw.org">EA UW–Madison</a>.</p>
+    </div>
   )
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Nav() {
   return (
-    <div id="nav"></div>
+    <div id="nav" className="flex justify-between items-center">
+      <Image
+        src="/waisi_gradient_rounded.png"
+        width={150}
+        height={150}
+        alt="WAISI logo"
+        className="flex items-center p-4"
+      />
+      <div id="links" className="flex items-center justify-end space-x-4 p-4">
+        <Link href="/" className="p-4">Home</Link>
+        <Link href="/about" className="p-4">About</Link>
+        <Link href="/events" className="p-4">Events</Link>
+        <Link href="/leadership" className="p-4">Leadership</Link>
+        <Link href="/projects" className="p-4">Projects</Link>
+      </div>
+    </div>
   )
 }
