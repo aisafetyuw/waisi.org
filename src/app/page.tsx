04fb43758918c07ea40f04d493dce4856760cbfb
Jeremy Kintana <81502213+jkintana@users.noreply.github.com>
2023-07-06 23:21:51 -0700

    Update footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div id="home">
       <h1>Wisconsin AI Safety Initiative</h1>
+      <p>We’re a student group conducting research to reduce risks from advanced AI.</p>
       <main>Hello, world!</main>
     </div>
   )
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,19 +4,20 @@ import Link from 'next/link'
 export default function Nav() {
   return (
     <div id="nav" className="flex justify-between items-center">
-      <Image
-        src="/waisi_gradient_rounded.png"
-        width={150}
-        height={150}
-        alt="WAISI logo"
-        className="flex items-center p-4"
-      />
-      <div id="links" className="flex items-center justify-end space-x-4 p-4">
-        <Link href="/" className="p-4">Home</Link>
-        <Link href="/about" className="p-4">About</Link>
-        <Link href="/events" className="p-4">Events</Link>
-        <Link href="/leadership" className="p-4">Leadership</Link>
-        <Link href="/projects" className="p-4">Projects</Link>
+      <div className="flex items-center p-8">
+        <Image
+          src="/waisi_gradient_rounded.png"
+          width={150}
+          height={150}
+          alt="WAISI logo"
+        />
+      </div>
+      <div id="links" className="flex items-center justify-end space-x-4 p-8">
+        <Link href="/" className="p-2 hover:font-bold">Home</Link>
+        <Link href="/about" className="p-2 hover:font-bold">About</Link>
+        <Link href="/events" className="p-2 hover:font-bold">Events</Link>
+        <Link href="/leadership" className="p-2 hover:font-bold">Leadership</Link>
+        <Link href="/projects" className="p-2 hover:font-bold">Projects</Link>
       </div>
     </div>
   )
