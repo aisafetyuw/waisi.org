9e291dd0d09e2afab14c1919a27f04466f4634fe
jkintana <jkintana@protonmail.com>
2023-07-12 18:28:48 -0500

    updated favicon and footer info

--- src/app/leadership/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Leadership() {
-  return (
-    <div id="leadership">Leadership</div>
-  )
-}
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,27 +9,27 @@ export default function Nav() {
 
   return (
     <div>
-      {/* <Image className="absolute z-0 opacity-30 max-h-40 py-6"
+      <Image className="absolute z-0 opacity-30 max-h-32 py-6 w-full"
         src="/Wave.png"
         width={1920}
         height={200}
-        alt="tet"
-      /> */}
+        alt=""
+      />
       <div id="nav" className="flex justify-between items-center">
         <div className="flex items-center p-6">
           <Image className="z-10"
             src="/waisi_gradient_rounded.png"
-            width={125}
-            height={125}
+            width={75}
+            height={75}
             alt="WAISI logo"
           />
         </div>
         <div id="links" className="flex items-center justify-end space-x-4 p-8 text-lg underline-offset-8 z-10">
-          <Link href="/" className={pathname=="/" ? "p-2 hover:font-bold underline" : "p-2 hover:font-bold"}>Home</Link>
-          <Link href="/about" className={pathname=="/about" ? "p-2 hover:font-bold underline" : "p-2 hover:font-bold"}>About</Link>
-          <Link href="/events" className={pathname=="/events" ? "p-2 hover:font-bold underline" : "p-2 hover:font-bold"}>Events</Link>
-          <Link href="/leadership" className={pathname=="/leadership" ? "p-2 hover:font-bold underline" : "p-2 hover:font-bold"}>Team</Link>
-          <Link href="/projects" className={pathname=="/projects" ? "p-2 hover:font-bold underline" : "p-2 hover:font-bold"}>Projects</Link>
+          <Link href="/" className={pathname=="/" ? "p-2 md:visible invisible underline" : "p-2 md:visible invisible"}>Home</Link>
+          <Link href="/about" className={pathname=="/about" ? "p-2 md:visible invisible underline" : "p-2 md:visible invisible"}>About</Link>
+          <Link href="/events" className={pathname=="/events" ? "p-2 md:visible invisible underline" : "p-2 md:visible invisible"}>Events</Link>
+          <Link href="/leadership" className={pathname=="/leadership" ? "p-2 md:visible invisible underline" : "p-2 md:visible invisible"}>Team</Link>
+          <Link href="/projects" className={pathname=="/projects" ? "p-2 md:visible invisible underline" : "p-2 md:visible invisible"}>Projects</Link>
         </div>
       </div>
     </div>
