e040b75d1e186af2c90859f7d7ffb65df4c454d7
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-07-11 19:17:22 -0500

    Initial content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,27 @@
+import Member from "@/components/member";
+import { MemberData } from "@/types";
+
 export default function About() {
+  const members: MemberData[] = []; // TODO populate
+
   return (
-    <div id="about">About</div>
+    <div id="about">
+      <h2>Our Mission</h2>
+      <div id="mission">
+        <p>
+
+        </p>
+      </div>
+      <h2>Getting Started</h2>
+      <div id="getting-started">
+        <p>
+          
+        </p>
+      </div>
+      <h2>Leadership Team</h2>
+      <div id="team">
+        {members.map(member => <Member member={member} />)}
+      </div>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <div id="home">
       <h1>Wisconsin AI Safety Initiative</h1>
-      <main>Hello, world!</main>
+      <div id="home-content">
+        <img />
+        <p>
+          <main>
+            We are a very ambitious but friendly group interested in building a community of people
+            collaborating together to mitigate the risks that increasingly capable <b>AI</b> brings to the world.
+            We refine each other's mental models of the risks and build the skills to solve them through <b>AI Alignment</b> and <b>AI Governance.</b>
+          </main>
+        </p>
+      </div>
     </div>
   )
 }
