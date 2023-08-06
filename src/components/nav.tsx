36fd129bd74fbc319a522c364c00a2ddbd8e8486
jkintana <jkintana@protonmail.com>
2023-08-06 16:32:23 -0700

    Added mobile nav menu

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,19 +3,40 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+
+interface MobileNavProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  pathname: string;
+}
+
+function MobileNav({open, setOpen, pathname}: MobileNavProps) {
+  return (
+    <div className={`md:hidden z-50 absolute top-0 left-0 h-screen w-screen bg-white transition-all duration-500 ease-in-out filter ${open ? "opacity-100 translate-y-2" : "opacity-0 -translate-y-2"}`}>
+        
+      <div className="flex flex-col justify-center items-center mt-28">
+        <a href="/" className={`text-2xl font-semibold my-4 ${pathname=="/" ? "underline" : ""}`}>Home</a>
+        <a href="/about" className={`text-2xl font-semibold my-4 ${pathname=="/about" ? "underline" : ""}`}>About</a>
+      </div>  
+    </div>
+  )
+}
 
 export default function Nav() {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false)
 
   return (
     <nav>
+      <MobileNav open={open} setOpen={setOpen} pathname={pathname}/>
       <Image className="absolute z-0 opacity-30 max-h-32 py-6 w-full"
         src="/Wave.png"
         width={1920}
         height={200}
         alt=""
       />
-      <div id="nav" className="flex justify-between items-center">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
         <div className="flex items-center p-6">
           <Image className="z-10"
             src="/waisi_gradient_rounded.png"
@@ -24,11 +45,18 @@ export default function Nav() {
             alt="WAISI logo"
           />
         </div>
-        <div id="links" className="flex items-center justify-end space-x-4 p-8 text-lg underline-offset-8 z-10">
-          <Link href="/" className={pathname=="/" ? "p-2 underline" : "p-2"}>Home</Link>
-          <Link href="/about" className={pathname=="/about" ? "p-2 underline" : "p-2"}>About</Link>
-          {/*<Link href="/events" className={pathname=="/events" ? "p-2 underline" : "p-2"}>Events</Link>*/}
-          {/*<Link href="/projects" className={pathname=="/projects" ? "p-2 underline" : "p-2"}>Projects</Link>*/}
+
+        <div className="flex items-center justify-end space-x-4 p-8 text-lg font-semibold underline-offset-8 z-50 md:flex hidden">
+          <Link href="/" className={`p-2 ${pathname=="/" ? "underline" : ""}`}>Home</Link>
+          <Link href="/about" className={`p-2 ${pathname=="/about" ? "underline" : ""}`}>About</Link>
+        </div>
+
+        <div className="w-4/5 md:hidden flex justify-end p-8 items-center">
+          <div className="group z-50 w-6 h-6 cursor-pointer flex-col justify-between items-center flex" onClick={() => { setOpen(!open) }}>
+            <span className={`h-1 w-full bg-black rounded-lg cursor-pointer transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-2.5" : ""}`} />
+            <span className={`h-1 w-full bg-black rounded-lg cursor-pointer transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+            <span className={`h-1 w-full bg-black rounded-lg cursor-pointer transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
+          </div>
         </div>
       </div>
     </nav>
