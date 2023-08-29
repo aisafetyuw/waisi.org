5bb37060bbdf5e5af00d0153fba817ab6855b1e8
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-08-29 17:52:49 -0600

    Remove EA UW reference

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="bottom-0 w-full p-8 bg-fuchsia-500 from-violet-500 bg-gradient-to-tr">
       <p className="text-center text-white">Contact us at <a className="font-bold underline" href="mailto:aisafetyuw@gmail.com">aisafetyuw@gmail.com</a>.</p>
-      <p className="text-center text-white">In collaboration with <a className="font-bold underline" href="https://eauw.org">EA UW–Madison</a>.</p>
     </footer>
   )
 }
