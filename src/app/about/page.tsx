e5f31cf2cfd64fee272b2f236c993c55574d6001
Ben Hayum <benhayum@gmail.com>
2023-09-01 22:20:32 -0500

    Wording changes of the about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -117,8 +117,11 @@ export default function About() {
         <h3>Let's take this seriously</h3>
         <p className="mb-4">
           We ought to take all of these downsides seriously, even the more hypothetical ones. They are a pretty clear and
-          research grounded extrapolation of where AI technology will go. The leaders of the top AI companies agree and
-          have signed on in agreement with the <a target="_blank" href="https://www.safe.ai/statement-on-ai-risk">Center for AI Safety's Statement on AI Risk</a> which reads: 
+          research grounded extrapolation of where AI technology will go. That's not to say that things necessarily must end up horribly bad. 
+          But, when the extrapolation looks how it looks right now, it's clear that there is work to be dome. 
+          The leaders of the top 3 AI companies of OpenAI, DeepMind, and Anthropic agree and
+          have signed on in agreement with the <a target="_blank" href="https://www.safe.ai/statement-on-ai-risk">Center for AI Safety's Statement on AI Risk</a> 
+          which reads: 
         </p>
         <p className="mb-4 text-xl text-gray-500">
           “Mitigating the risk of extinction from AI should be a global priority alongside other societal-scale risks such as pandemics and nuclear war.”
