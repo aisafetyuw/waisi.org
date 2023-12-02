dfe75e1af41dd7f1c3b0eca169d19782d6516451
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-12-02 15:46:16 -0600

    Remove event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,6 @@ export default function Home() {
     <div id="home" className="page">
       <h1 className="text-gradient text-5xl font-extrabold py-4">Wisconsin AI Safety Initiative</h1>
       <div id="home-content">
-        <p id="headline" className="mb-4">
-          Join us on November 28th at 6:00 P.M. as we host Thomas Broadley
-          from <a href="https://evals.alignment.org/">ARC Evals</a>!
-          RSVP <a href="https://docs.google.com/forms/d/e/1FAIpQLSdYBui_GSLBoZXUL9IPWWxjJC-yE1NfNbTGYF-WYfnVICndVg/viewform">here</a> to
-          reserve your spot and to receive an email with location details once they are confirmed.
-        </p>
-        <AboutImage filename={'danger'} alt={'Detecting Danger in Artificial Intelligence'} />
         <main>
           <h3 className="mb-4">
             We're a community aiming to <b>mitigate the risks</b> that increasingly capable <b>artificial intelligence</b> brings to the world.
