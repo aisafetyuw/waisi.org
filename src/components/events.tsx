7dda09d8f06f5993935256b270eb134177595fbe
David Viggiano <dviggiano@wisc.edu>
2024-02-27 20:33:45 -0600

    Add backup events content

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -6,18 +6,21 @@ export default async function Events() {
 
   return (
     <div id="events">
-      {events && events.length > 0 &&
         <div>
           <h2>Upcoming Events</h2>
-          <ul className="md:w-5/6">
-            {events.map((event, index) => (
-              <li key={index}>
-                <CalendarEvent key={index} event={event} />
-              </li>
-            ))}
-          </ul>
+          {events && events.length > 0 ?
+            <ul className="md:w-5/6">
+              {events.map((event, index) => (
+                <li key={index}>
+                  <CalendarEvent key={index} event={event} />
+                </li>
+              ))}
+            </ul> :
+            <p className='bold text-xl'>
+              Nothing outside of our usual programming happening soon &#8212; check back later!
+            </p>
+          }
         </div>
-      }
     </div>
   )
 }
