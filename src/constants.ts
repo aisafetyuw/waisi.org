b971b072ee4f3b5ccfd09ae98ac53702911fe682
David Viggiano <dviggiano@wisc.edu>
2024-04-26 19:33:08 -0500

    Update handbook buttons

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const INSTAGRAM_URL = 'https://www.instagram.com/aisafetyuw/';
 export const LINKEDIN_URL = 'https://www.linkedin.com/company/waisi/';
 export const CULTURE_STATEMENT = 'https://docs.google.com/document/d/1KUVD7c-ZM0uijD0RvYYk6yBl0Oe09AAgIvYyoVUj4h8/edit?usp=sharing';
 export const Handbooks = {
-    AT_A_GLANCE: "https://docs.google.com/document/d/18wiWtYgUxwXKSxChBOuQAittTNzXxSUaagsz-43Nt14/edit?usp=sharing",
-    FELLOWSHIP: "https://docs.google.com/document/d/1nlA0I7yGo7e7tC6xS8kBQUuyil88xrYji5KPeyUPRwA/edit?usp=sharing",
-    SAFETY_SCHOLARS: "https://docs.google.com/document/d/1npT_7Svl_Gp10-BV8yusx6A90OokOflWFgCagKKU0G8/edit?usp=sharing",
+    AT_A_GLANCE: 'https://docs.google.com/document/d/18wiWtYgUxwXKSxChBOuQAittTNzXxSUaagsz-43Nt14/edit?usp=sharing',
+    FELLOWSHIP: 'https://docs.google.com/document/d/1nlA0I7yGo7e7tC6xS8kBQUuyil88xrYji5KPeyUPRwA/edit?usp=sharing',
+    SAFETY_SCHOLARS: 'https://docs.google.com/document/d/1npT_7Svl_Gp10-BV8yusx6A90OokOflWFgCagKKU0G8/edit?usp=sharing',
 };
