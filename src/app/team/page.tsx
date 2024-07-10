943a8043bd427a0e77a377ef30f149bbbdd9462f
Alka Lakadia <alkalakadia@Alkas-MacBook-Pro-2.local>
2024-07-10 11:20:16 +0530

    Added new leader headshots to public/leaders and updated src/team

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -15,6 +15,14 @@ export default async function Team() {
           </li>
         ))}
       </ul>
+      <h3>Research Team</h3>
+      <ul id="team-list" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 m-1 mb-8">
+        {members.filter(member => member.team === '3').map((member, index) => (
+          <li key={index} className="col-span-1 h-full bg-white">
+            <Member member={member} />
+          </li>
+        ))}
+      </ul>
       <h3>Technical Team</h3>
       <ul id="team-list" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 m-1 mb-8">
         {members.filter(member => member.team == '1').map((member, index) => (
