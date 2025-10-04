bc4a067fbc46f20b034f761c691a69da703b8132
jkintana <jkintana@protonmail.com>
2025-10-04 16:49:03 -0500

    Attempt with Claude Code + Serena + Context7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Open_Sans, DM_Serif_Display } from 'next/font/google'
+import { Suspense } from 'react'
 import Nav from '../components/nav';
 import Footer from '../components/footer';
 
@@ -22,7 +23,9 @@ export default function RootLayout({
         <script src="https://kit.fontawesome.com/8a0618493f.js" crossOrigin="anonymous"></script>
       </head>
       <body className={`${font.className} ${dmSerifDisplay.variable}`}>
-        <Nav />
+        <Suspense fallback={<div className="fixed top-0 left-0 right-0 z-50 h-20" style={{backgroundColor: '#FFF9F0'}} />}>
+          <Nav />
+        </Suspense>
         <div id="content" className="flex flex-col min-h-screen pt-20"  style={{ minHeight: 'calc(100vh - 210px)' }}>
           {children}
         </div>
